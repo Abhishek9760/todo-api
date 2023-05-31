d293b8fd80136b29362d2f04b0990711a6fd312a
Abhishek Verma <av4programming@gmail.com>
2023-05-31 14:56:17 +0530

    changes done

--- todo/api/urls.py
+++ todo/api/urls.py
@@ -6,7 +6,5 @@ app_name = "status"
 urlpatterns = [
     path('', TodoListView.as_view(), name='todo-list'),
     path('<int:pk>/', TodoDetailView.as_view(), name='todo-detail'),
-    # path('<int:id>/update/', StatusUpdateAPIView.as_view()),
-    # path('<int:id>/delete/', StatusDeleteAPIView.as_view()),
 ]
 
